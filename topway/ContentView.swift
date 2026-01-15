a65b4b16ad28b928df9bd2dd06e34c12d58f1b0e
pulgueta <retardix456@gmail.com>
2026-01-15 01:12:48 -0500

    Initial Commit

---
+++ topway/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  topway
+//
+//  Created by Andrés Rodríguez on 1/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ topway/topwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  topwayApp.swift
+//  topway
+//
+//  Created by Andrés Rodríguez on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct topwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
